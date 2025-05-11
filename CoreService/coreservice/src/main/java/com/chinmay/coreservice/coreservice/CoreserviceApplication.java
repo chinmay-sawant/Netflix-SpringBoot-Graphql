5eb3a0585486b6a4602cb5b54e077ac4c6c24bd7
Chinmay Sawant <sawantchinmay040@gmail.com>
2025-05-12 00:33:13 +0530

    Init Commit

---
+++ CoreService/coreservice/src/main/java/com/chinmay/coreservice/coreservice/CoreserviceApplication.java
@@ -0,0 +1,13 @@
+package com.chinmay.coreservice.coreservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreserviceApplication.class, args);
+	}
+
+}
---
+++ CoreService/coreservice/src/test/java/com/chinmay/coreservice/coreservice/CoreserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chinmay.coreservice.coreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
